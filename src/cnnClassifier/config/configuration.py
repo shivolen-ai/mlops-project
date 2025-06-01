c0dfe0a16ac8ff179afea587c2a3c07907532211
Srujan <srujanrkumar@gmail.com>
2025-06-02 01:02:50 +0530

    done

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -3,7 +3,8 @@ from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
-                                                TrainingConfig)
+                                                TrainingConfig,
+                                                EvaluationConfig)
 
 
 class ConfigurationManager:
@@ -76,4 +77,14 @@ class ConfigurationManager:
 
         return training_config
     
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data=r"C:\Users\srujanrk\Desktop\mlops-project-folder\mlops-project\artifacts\data_ingestion\CNRPark-Patches-150x150",
+            mlflow_uri="https://dagshub.com/srujank18/mlops-project.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
 
